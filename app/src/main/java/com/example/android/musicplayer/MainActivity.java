7a6917fd9dd0349b461e8068e341f212ac241195
Jaren831 <jaren.lynchmobb@gmail.com>
2016-06-27 00:08:13 -0700

    Got structure for music player. Two activities. - Main activity is in library. Displays song selection. Selecting a song will take user to player Activity. - Player activity displays artist/song/album art. View bar based on calculating with time remaining. - Mediaplayer class will handle audio playback. - Back/Forward arrows redisplay Player activity for new song. - Pause/play button - All click events are setonclick

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView song = (TextView) findViewById(R.id.song);
+        // Set a click listener on that View
+        song.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@Link FamilyActivity}
+                Intent songIntent = new Intent(MainActivity.this, PlayerActivity.class);
+                startActivity(songIntent);
+            }
+        });
     }
 }
